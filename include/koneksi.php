36dfa4025c8954d236101038af42305fc19a9f91
BillyTitanPratama <122201902730@mhs.dinus.ac.id>
2020-07-11 19:22:27 +0700

    first commit

---
+++ include/koneksi.php
@@ -0,0 +1,10 @@
+<?php
+$server = "sql105.epizy.com";
+$username = "epiz_26212084";
+$password = "oq96Il9ew6IsAcJ";
+$database = "epiz_26212084_laundry";
+
+$conn = mysqli_connect($server, $username, $password, $database);
+
+//mysqli_select_db($database) or die("Datebase tidak bisa dibuka");
+?>
